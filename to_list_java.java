1b6e543b3bd5270060a9b9840844a0396a8a0c96
Uttam <shettyuttam010@gmail.com>
2026-06-16 14:51:47 +0530

    new

--- to_list_java.java
+++ to_list_java.java
@@ -20,7 +20,7 @@ public class to_list_java {
             System.out.println("\n==== TO-DO LIST ====");
             System.out.println("1. Add Task");
             System.out.println("2. View Tasks");
-            System.out.println("3. Mark Task Completed");
+            System.out.println("3. Mark Task ");
             System.out.println("4. Delete Task");
             System.out.println("5. Exit");
             System.out.print("Choose option: ");
